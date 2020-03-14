332ec8348db6c0cc15301b4d26e124e39d629795
Modhusudan Mallick <37875645+Kali5@users.noreply.github.com>
2020-03-14 18:38:04 +0600

    Add files via upload

---
+++ main.js
@@ -0,0 +1,27 @@
+var monthName = "April";
+var days = 30;
+var startingDay = 0;
+
+
+console.log("\n\nCalender Of",monthName,"\n");
+console.log("sun mon tue wed thu fri sat");
+
+for(var i=0;i<5;i++){
+	var dayRow ="";
+	for(var j=1;j<=7;j++){
+		var currentDay = 7*i + j - startingDay;
+
+		if(currentDay>days){
+			break;
+		}else if (currentDay < 1){
+			currentDay = " ";
+		}
+
+		if(currentDay>9){
+			dayRow += currentDay + "	";
+		}else{
+			dayRow += currentDay + "	";
+		}
+	}
+	console.log(dayRow);
+}
